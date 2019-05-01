2a0fdfe2c8d6ecb0835ef26e030785578bac41e5
VernonDodo <vcdodo@gmail.com>
2019-04-30 20:28:02 -0400

    Code changes to the connection and index files

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
+console.log(process.env.MONGODB_URI)
 // connect to the database
 if (process.env.MONGODB_URI) {
     mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-    mongoose.connect('mongodb://localhost:3000/credit-manager');
+    mongoose.connect('mongodb://localhost/credit-manager', { useNewUrlParser: true});
 }
 
 mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: '+ error);
+    console.error('MongoDB connection error: '+ err);
     process.exit(-1);
 });
 
--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,21 @@ const recordController = require("../controllers/records")
 
 /* The following routes are all the user routes */
 
-router.get('/', userController.index)
-router.get('/', userController.getUser)
-router.post('/', userController.create)
+router.get('/users', userController.index)
+router.get('/users/:id', userController.getUser)
+router.post('/users', userController.create)
 
 /* The following are all the account routes */
-router.get('/', accountController.getAccounts)
-router.get('/', accountController.getAccount)
-router.post('/', accountController.update)
-router.post('/', accountController.delete)
+router.get('/accounts', accountController.getAccounts)
+router.get('/accounts/:id', accountController.getAccount)
+router.post('/accounts/:d', accountController.update)
+router.post('/accounts', accountController.delete)
 
 /* The following are all the record routes */
-router.get('/', recordController.getRecord)
-router.get('/', recordController.getRecords)
-router.post('/', recordController.update)
-router.post('/', recordController.create)
+router.get('/records/:id', recordController.getRecord)
+router.get('/records', recordController.getRecords)
+router.post('/records/:id', recordController.update)
+router.post('/records', recordController.create)
 
 
 module.exports = router
